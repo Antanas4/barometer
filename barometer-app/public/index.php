ac3292b39a9d82cd7477e82f55fb021992f5890b
Antanas4 <antanas.buteliauskas@gmail.com>
2024-12-04 16:27:21 +0200

    resetting

--- barometer-app/public/index.php
+++ barometer-app/public/index.php
@@ -13,5 +13,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
+<<<<<<< HEAD
 // (require_once __DIR__.'/../bootstrap/app.php')
 //     ->handleRequest(Request::capture());
+=======
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
+>>>>>>> cd42c82 (setting up the laravel + react app)
